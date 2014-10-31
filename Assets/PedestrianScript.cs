b4659cb7b35d188bf2f13a45d1f325447fa0340f
Joe Strong <joe@freshleafmedia.co.uk>
2014-10-31 13:43:22 +0000

    Added crazy pedestrians

---
+++ Assets/PedestrianScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PedestrianScript : MonoBehaviour {
+
+	public GameObject target;
+	Rigidbody body;
+
+	void Start() {
+		body = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		Vector3 newPosition = body.position + transform.forward * 1f * Time.deltaTime;
+		body.MovePosition (newPosition);
+	}
+}
---
+++ Assets/PedestrianSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PedestrianSpawner : MonoBehaviour {
+
+	public GameObject[] spawnPoints;
+	public GameObject spawnObject;
+	public float spawnTime = 3f;
+
+	void Start()
+	{
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+
+	void Spawn()
+	{
+		GameObject spawnPoint = spawnPoints [Random.Range (0, spawnPoints.Length)];
+		Instantiate (spawnObject, spawnPoint.transform.position, spawnPoint.transform.rotation);
+	}
+
+}
